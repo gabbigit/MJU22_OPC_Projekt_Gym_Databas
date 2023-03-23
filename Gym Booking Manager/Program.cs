b1f507f9363d65c19aea7b971369ae2030324ae5
Gabriella <gabivrban@gmail.com>
2023-03-23 11:57:57 +0100

    - nuget Npgsql - a start of DBStorage class

--- Gym Booking Manager/Program.cs
+++ Gym Booking Manager/Program.cs
@@ -2,6 +2,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
+using Npgsql;
 
 #if DEBUG
 [assembly: InternalsVisibleTo("Tests")]
